52ce9b8118eadb186dc8df92c3db1a8b788cf0d2
Marllon Campos <52542050+MarllonCampos@users.noreply.github.com>
2023-09-08 19:21:42 -0300

    feat: add cache-control no-cache and create a new function that will call the  update route

--- web/src/services/api.ts
+++ web/src/services/api.ts
@@ -1,9 +1,21 @@
 import { fileState } from '../components/Upload';
 
 export async function validateFile(body: fileState) {
-  return await fetch('http://localhost:3333/', {
+  return await fetch('http://localhost:3333/validate', {
     headers: {
       'content-type': 'application/json',
+      'Cache-Control': 'no-cache',
+    },
+    method: 'POST',
+    body: JSON.stringify(body),
+  });
+}
+
+export async function update(body: fileState) {
+  return await fetch('http://localhost:3333/action', {
+    headers: {
+      'content-type': 'application/json',
+      'Cache-Control': 'no-cache',
     },
     method: 'POST',
     body: JSON.stringify(body),
